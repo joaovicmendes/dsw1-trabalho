0369ad1c451d6759fab86b71701591347661adc2
João Victor Mendes <mendesjoaovic@gmail.com>
2020-11-12 19:20:24 -0300

    Exercício 1

---
+++ Exercício 1/CelciusToFahrenheit/src/main/java/br/ufscar/dc/dsw/CelciusToFahrenheit.java
@@ -0,0 +1,65 @@
+package br.ufscar.dc.dsw;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/Conversion" })
+public class CelciusToFahrenheit extends HttpServlet {
+    
+private static final long serialVersionUID = 1L;
+    
+protected void processRequest(HttpServletRequest request, HttpServletResponse response, String metodo) throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    try (PrintWriter out = response.getWriter()) {
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Conversão ˚C para ˚F</title>");
+        out.println("<meta charset=\"UTF-8\">");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Conversão Celcius para Fahrenheit</h1>");
+
+        String minValue = request.getParameter("min");
+        int min = (minValue == null) ? -100 : Integer.parseInt(minValue);
+        String maxValue = request.getParameter("max");
+        int max = (maxValue == null) ?  100 : Integer.parseInt(maxValue);
+        String stepValue = request.getParameter("step");
+        int step = (stepValue == null) ?  5 : Integer.parseInt(stepValue);
+
+        out.println("<table>");
+        out.println("<tr>");
+        out.println("<th>Celsius</th>");
+        out.println("<th>Fahrenheit</th>");
+        out.println("</tr>");
+        for (int celsius = min; celsius <= max; celsius += step) {
+            double fahr = 1.8 * celsius + 32;
+            out.println("<tr>");
+            out.println("<td>" + celsius + "</td>");
+            out.println("<td>" + fahr + "</td>");
+            out.println("</tr>");
+        }
+        out.println("</table>");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+        }
+           
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    processRequest(request, response, "GET");
+}
+           
+@Override
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    processRequest(request, response, "POST");
+}
+}
---
+++ Exercício 1/README.md
@@ -0,0 +1,8 @@
+# Desenvolvimento de Software para Web (1001504) - Exercício 1
+
+Universidade Federal de São Carlos – UFSCar \
+Departamento de Computação \
+1001504 - Desenvolvimento de Software para Web 1 – Exercício 1 \
+Prof. Dr. Delano Medeiros Beder \
+Aluno: João Victor Mendes Freire \
+RA: 758943
