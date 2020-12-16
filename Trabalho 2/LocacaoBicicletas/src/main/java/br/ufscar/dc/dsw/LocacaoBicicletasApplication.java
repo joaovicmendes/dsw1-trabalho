6a1442b1caa835705d64b7671b2e6f880ee1dd35
João Victor Mendes <mendesjoaovic@gmail.com>
2020-12-16 11:33:03 -0300

    Projeto base do Trabalho 2

---
+++ Trabalho 2/LocacaoBicicletas/src/main/java/br/ufscar/dc/dsw/LocacaoBicicletasApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocacaoBicicletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocacaoBicicletasApplication.class, args);
+	}
+
+}
